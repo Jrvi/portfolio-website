f6b07effc2107749508578fabd02d4ab42e5df8c
Juho Järvi <juho.jaervi@gmail.com>
2024-08-25 11:23:12 +0300

    chore: added text to home

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -2,8 +2,16 @@ const Home = () => {
     return (
       <div>
         <h1>Home</h1>
-        <h2>Kuka olen</h2>
+        <h3>Kuka olen</h3>
         <p>Hei, Olen Juho Järvi ja olen Jyväskylän yliopiston Teknologiajohtamisen opiskelija.</p>
+        <h3>Esittelyteksti</h3>
+        <p>Olen kokenut Software Developer, jolla on taustaa Backend devaajana. Olen työskennellyt monenlaisten projektien parissa, Verkkokauppa ja portaali projekteissa, ja intohimonani on tuoda esiin asiakkaiden ainutlaatuisuus ja brändi.</p>
+        <h3>Call to Action</h3>
+        <p>Kiinnostuitko? Tutustu tarkemmin töihini tai ota yhteyttä keskustellaksemme, kuinka voin auttaa sinua toteuttamaan ideasi.</p>
+        <h3>Yhteystiedot</h3>
+        <p>Sähköposti</p>
+        <p>LinkedIn</p>
+        <p>Github</p>
       </div>
     )
   }
